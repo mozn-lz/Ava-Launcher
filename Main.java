454e8dee030c151981f6593e6e0ff1a105981909
Moeketsane SEFAKO <msefako@c2r4s6.wethinkcode.co.za>
2019-07-05 17:24:57 +0200

    minor cleanup in main

--- Main.java
+++ Main.java
@@ -26,30 +26,24 @@ public class Main {
 								simLoop = Integer.valueOf(line);
 								System.out.println(line);
 							} catch (IllegalArgumentException ex) {
-								System.out.println("First line is not an integer number");
+								System.out.println("The first line is not a number.\n Exiting...");
+								System.exit(0);
 							}
 						} else if (fileLineCount > 0) {
 							String[] parts = line.split(" ");
 							aircraftList.add(factory.newAircraft(parts[0], parts[1], Integer.valueOf(parts[2]), Integer.valueOf(parts[3]), Integer.valueOf(parts[4])));
-							// create checker to verify if parts[2-4] are ints
-
-							// continue
-								// aircraftList.get();
-							// lets stop here
 						}
-	// System.out.println("\n\n");
 						fileLineCount++;
-					} else {
-						//System.out.println("Line is -- NULL");
 					}
 				}
 				reader.close();
 				weatherTower.simControler(aircraftList, simLoop);
 			} catch (IOException e) {
+				System.out.print("File reading error: ");
 				e.printStackTrace();
 			}
 		} else {
-			System.out.println("Argument error: number og arguments is not 1.");
+			System.out.println("Argument error: number of arguments is not 1.");
 		}
 	}
 
