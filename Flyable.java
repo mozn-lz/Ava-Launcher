99b71d240f6f85ca7631dbea93899ddb8dee91c6
mozn-lz <mozn.lozn2000@gmail.com>
2019-07-07 23:56:03 +0200

    Deleted java files fr om root(moved to src)

--- Flyable.java
+++
@@ -1,6 +0,0 @@
-
-
-public interface Flyable {
-	void updateConditions();
-	void registerTower(WeatherTower weatherTower);
-}
--- Tower.java
+++
@@ -1,7 +0,0 @@
-
-
-public abstract class Tower {
-		public abstract void register(Flyable flyable);
-		public abstract void unregister(Flyable flyable);
-		public abstract void conditionChanged();
-}
