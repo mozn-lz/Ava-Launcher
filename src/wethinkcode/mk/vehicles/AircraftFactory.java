a6cde427476bf1be10a1872bece8759858729406
Moeketsane SEFAKO <msefako@c2r4s6.wethinkcode.co.za>
2019-07-09 18:40:38 +0200

    fixed something 2 hours ago *forgot

--- src/wethinkcode/mk/vehicles/AircraftFactory.java
+++ src/wethinkcode/mk/vehicles/AircraftFactory.java
@@ -13,26 +13,22 @@ public class AircraftFactory {
 	}
 
 	public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
-
-		if (type == "JetPlane" || type == "Baloon" || type == "Helicopter") {
-			switch (type) {
-				case "JetPlane":
-					JetPlane plane = new JetPlane(name, new Coordinates(longitude, latitude, height));
-					return plane;
-				case "Baloon":
-					Baloon baloon = new Baloon(name, new Coordinates(longitude, latitude, height));
-					return baloon;
-				case "Helicopter":
-					Helicopter helicopter = new Helicopter(name, new Coordinates(longitude, latitude, height));
-					return helicopter;
-				default:
-					System.out.println("Error: Flyable type is not recognized.");
-					System.exit(0);
-					return null;
+		switch (type) {
+			case "JetPlane":
+				JetPlane plane = new JetPlane(name, new Coordinates(longitude, latitude, height));
+				return plane;
+			case "Baloon":
+				Baloon baloon = new Baloon(name, new Coordinates(longitude, latitude, height));
+				return baloon;
+			case "Helicopter":
+				Helicopter helicopter = new Helicopter(name, new Coordinates(longitude, latitude, height));
+				return helicopter;
+			default:
+				System.out.println("Error: Flyable type is not recognized.");
+				System.out.println("Error: '" + type + "' is not a valid aircraft type");
+				// System.exit(0);
+				return null;
 			}
-		} else {
-			System.out.println("Error: '" + type + "' is not a valid aircraft type");
+			// return null;
 		}
-		return null;
-	}
 }
